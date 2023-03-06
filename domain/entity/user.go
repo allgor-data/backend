82255258039830e00b521f199a981265e1ad0eb7
dominickbrasileiro <brasileiro.dominick@gmail.com>
2023-03-06 09:47:56 -0300

    feat: add user entity

---
+++ domain/entity/user.go
@@ -0,0 +1,128 @@
+package model
+
+import (
+	"errors"
+	"fmt"
+	"net/mail"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type UserRole = string
+
+const (
+	UserRoleAdmin    UserRole = "admin"
+	UserRoleOperator UserRole = "operator"
+)
+
+type User struct {
+	UID          string
+	Email        string
+	FirstName    string
+	LastName     string
+	PasswordHash string
+	Role         UserRole
+}
+
+func NewUser() *User {
+	return &User{
+		UID:  uuid.New().String(),
+		Role: UserRoleOperator,
+	}
+}
+
+func (u *User) IsValid() error {
+	err := u.validateEmail()
+	if err != nil {
+		return err
+	}
+
+	err = u.validateFirstName()
+	if err != nil {
+		return err
+	}
+
+	err = u.validateLastName()
+	if err != nil {
+		return err
+	}
+
+	err = u.validatePasswordHash()
+	if err != nil {
+		return err
+	}
+
+	err = u.validateRole()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (u *User) SetPassword(password string) error {
+	const MIN_LENGTH = 8
+
+	if len(password) < MIN_LENGTH {
+		return fmt.Errorf("password length must be greater or equal than %d", MIN_LENGTH)
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+
+	u.PasswordHash = string(hash)
+
+	return nil
+}
+
+func (u *User) validateEmail() error {
+	if u.Email == "" {
+		return errors.New("e-mail is required")
+	}
+
+	_, err := mail.ParseAddress(u.Email)
+	if err != nil {
+		return errors.New("invalid e-mail")
+	}
+
+	return err
+}
+
+func (u *User) validateFirstName() error {
+	if u.FirstName == "" {
+		return errors.New("first name is required")
+	}
+
+	return nil
+}
+
+func (u *User) validateLastName() error {
+	if u.LastName == "" {
+		return errors.New("last name is required")
+	}
+
+	return nil
+}
+
+func (u *User) validatePasswordHash() error {
+	if u.PasswordHash == "" {
+		return errors.New("password is not set")
+	}
+
+	return nil
+}
+
+func (u *User) validateRole() error {
+	if u.Role == "" {
+		return errors.New("role is required")
+	}
+
+	if u.Role != UserRoleAdmin && u.Role != UserRoleOperator {
+		return errors.New("invalid role")
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -5,10 +5,13 @@ go 1.19
 require (
 	github.com/99designs/gqlgen v0.17.25
 	github.com/cosmtrek/air v1.42.0
+	github.com/google/uuid v1.3.0
 	github.com/joho/godotenv v1.5.1
 	github.com/rs/zerolog v1.29.0
 	github.com/urfave/cli/v2 v2.24.4
 	github.com/vektah/gqlparser/v2 v2.5.1
+	golang.org/x/crypto v0.7.0
+	gotest.tools/v3 v3.4.0
 )
 
 require (
@@ -17,6 +20,7 @@ require (
 	github.com/creack/pty v1.1.18 // indirect
 	github.com/fatih/color v1.14.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/google/go-cmp v0.5.5 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.1 // indirect
 	github.com/imdario/mergo v0.3.13 // indirect
@@ -28,8 +32,8 @@ require (
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/mod v0.8.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
 	golang.org/x/tools v0.6.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
