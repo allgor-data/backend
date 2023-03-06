429560e768ce17f4ce4f0a0c50510e49157876bd
dominickbrasileiro <brasileiro.dominick@gmail.com>
2023-03-06 10:28:57 -0300

    feat: add in memory user repository

---
+++ domain/repository/in_memory.go
@@ -0,0 +1,22 @@
+package repository
+
+import (
+	"github.com/allgor-data/backend/domain/entity"
+)
+
+type InMemoryUserRepository struct {
+	users []*entity.User
+}
+
+var _ UserRepository = (*InMemoryUserRepository)(nil)
+
+func NewInMemoryUserRepository() *InMemoryUserRepository {
+	return &InMemoryUserRepository{
+		users: make([]*entity.User, 0),
+	}
+}
+
+func (i *InMemoryUserRepository) CreateUser(user *entity.User) error {
+	i.users = append(i.users, user)
+	return nil
+}
